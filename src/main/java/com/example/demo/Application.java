17da757e3adc7758eafb7a0ae694f5db611f91c7
amitharidwar <amitkumarakshat@gmail.com>
2025-04-19 21:20:36 +0530

    change

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class git Application {
+public class Application {
 
     private static final Logger logger = LoggerFactory.getLogger(Application.class);
 
